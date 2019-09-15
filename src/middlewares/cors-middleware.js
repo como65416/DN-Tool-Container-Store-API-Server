4ec6843b288ce60f310bebe391d94fe8da3d389f
comoco <como65416@gmail.com>
2019-09-15 23:35:41 +0800

    fix cors-middleware for no origin header

--- src/middlewares/cors-middleware.js
+++ src/middlewares/cors-middleware.js
@@ -1,10 +1,10 @@
 
 function corsMiddleware (req, res, next) {
-  let enableCors = process.env.ENABLE_CORS;
-  let availableDomains = process.env.AVAILABLE_DOMAINS.split(',');
+  let enableCors = process.env.ENABLE_CORS || 'false';
+  let availableDomains = process.env.AVAILABLE_DOMAINS.split(',') || [];
+  let url = (req.get('origin')) ? new URL(req.get('origin')) : null;
 
-  let url = new URL(req.get('origin'));
-  if (enableCors == 'true' && availableDomains.includes(url.host)) {
+  if (enableCors == 'true' && url != null && availableDomains.includes(url.host)) {
     res.header('Access-Control-Allow-Origin', req.get('origin'));
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
