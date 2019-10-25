9d17601e3bd72152fb4667668ac90d488b1fd5a8
comoco <como65416@gmail.com>
2019-10-25 18:03:30 +0800

    update  install.js

--- install.js
+++ install.js
@@ -67,7 +67,6 @@ inquirer.prompt([
     })
     .catch((e) => {
       console.log('\x1b[31m%s\x1b[0m', 'Install database fail :');
-      console.log('    ' + e.message);
     });
 })
 .catch(err => {
